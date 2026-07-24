ce6eb8a15730f585d9ca12dd63faca53e87e6b50
Khoiru Miftakhul Falah <mrcr4zysm1le@gmail.com>
2026-07-24 15:04:12 +0700

    fixing logo navbar

--- src/layouts/PublicLayout/Navbar.tsx
+++ src/layouts/PublicLayout/Navbar.tsx
@@ -65,7 +65,7 @@ const Navbar: React.FC = () => {
             <img
               src="assets/img/Content/logo.png"
               alt="Logo"
-              width={34}
+              width={100}
               height={34}
             />
           </div>
